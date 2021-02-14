7c9e3f3c61df368452461281249e45717b6c7da6
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-02-14 11:07:28 -0500

    maybe commit to github is necessary to make work. added background

---
+++ build/precache-manifest.e445e8502db9f5d66200f59aef595e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31661fe2d06a58d0ce4665ccc942b424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb41c48b02b3c6a5b3f",
+    "url": "/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "0cb41c48b02b3c6a5b3f",
+    "url": "/static/js/2.82294235.chunk.js"
+  },
+  {
+    "revision": "b962909a7040b53b76bb931dd59e4242",
+    "url": "/static/js/2.82294235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b1df2ad16e364ad77a",
+    "url": "/static/js/main.fbe25604.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
