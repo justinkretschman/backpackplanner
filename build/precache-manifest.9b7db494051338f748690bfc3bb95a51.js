85a61d314a06ca886c009ea3034a1b2d1bf92a25
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-02-13 21:34:17 -0500

    correct firebase project

---
+++ build/precache-manifest.9b7db494051338f748690bfc3bb95a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261454282d99c50a957dbf43a4f07779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fed1e7b95bd0b85ddd9",
+    "url": "/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "1fed1e7b95bd0b85ddd9",
+    "url": "/static/js/2.fa88ffca.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.fa88ffca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d2ef9ac9df2d6a1dff",
+    "url": "/static/js/main.d287913d.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
