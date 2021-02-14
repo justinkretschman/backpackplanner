e68c7c52d82bb173275d9c28fd283ca69ca1f0af
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-02-14 17:50:53 -0500

    ifdk goodbye

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,10 @@ const config = {
   apiKey: "AIzaSyAbyznhjI_UAbo8CIjcqEP-GYjfQxtCtL4",
   authDomain: "peeoutdoors.firebaseapp.com",
   projectId: "peeoutdoors",
+  storageBucket: "peeoutdoors.appspot.com",
+  messagingSenderId: "967116705317",
+  appId: "1:967116705317:web:3bf644f7f65b24446639cc",
+  measurementId: "G-PBXFJMYCL7",
   };
 firebase.initializeApp(config);
 
