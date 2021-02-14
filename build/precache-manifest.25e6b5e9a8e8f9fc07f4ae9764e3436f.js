2bbe6776ecbf15575fc22320345cdb39cbc6a477
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-02-14 11:53:41 -0500

    connect fb and add free membership for life

---
+++ build/precache-manifest.25e6b5e9a8e8f9fc07f4ae9764e3436f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf1ad612bc44aa8e3e156c3fd85e8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5339bcb003d488b40251",
+    "url": "/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "5339bcb003d488b40251",
+    "url": "/static/js/2.e47cb5b1.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.e47cb5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718e4816eba7586f445b",
+    "url": "/static/js/main.4647c530.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,17 @@ import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
-    <Container
+    <div style={{ 
+      backgroundImage: `https://www.reserveamerica.com/webphotos/racms/articles/images/c58ec5b6-63d5-47c8-afd3-0085a082eb68_image2_backpacker-hero-image.jpg")` 
+    }}>
+      <Container
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
     >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <h2 className="text-center mb-4">Free membership for life if you join now!</h2>
+        </div> 
+
       <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
           <AuthProvider>
@@ -29,6 +36,7 @@ function App() {
         </Router>
       </div>
     </Container>
+    </div>
   )
 }
 
